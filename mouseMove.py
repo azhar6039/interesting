4cb4e4d0f3d979a10cc5cf84e3c41da5f8cedffb
Shaik Azhar <48443441+azhar6039@users.noreply.github.com>
2022-10-24 11:47:47 +0530

    Create mouseMove.py

---
+++ mouseMove.py
@@ -0,0 +1,9 @@
+#keep mouse moving continously to keep teams active
+import pyautogui as pygui
+print(pygui.size())
+import time
+while True:
+    pygui.moveTo(700,200, duration = 2)
+    time.sleep(10)
+    pygui.moveTo(400,300, duration = 2)
+    #pygui.scroll(-100)
